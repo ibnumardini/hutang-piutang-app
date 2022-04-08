e3c35d07475c15e774192bfcf9044d4c54eab0de
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-04-08 08:48:26 +0700

    add: adding stylesheet

--- app/templates/header.php
+++ app/templates/header.php
@@ -7,5 +7,6 @@
     <title><?=$page ?? 'Dashboard'?> | Hutang Piutang App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
