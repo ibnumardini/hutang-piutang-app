1a6747bcf8fd4f1925876a0ccad2d0403f0ec052
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-04-08 10:13:53 +0700

    add: layout transactions

--- app/pages/transactions.php
+++ app/pages/transactions.php
@@ -1,3 +1,40 @@
 <div class="container">
-    <h1>Transactions</h1>
+    <h1 class="mb-3">Transactions</h1>
+
+    <table class="table table-striped table-bordered">
+        <thead class="table-dark">
+            <tr>
+                <th>No</th>
+                <th>Untuk</th>
+                <th>Orang</th>
+                <th>Nominal</th>
+                <th>Status</th>
+                <th>Jatuh Tempo</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>membeli salome</td>
+                <td>wilkiwil</td>
+                <td>Rp. 5.000,00</td>
+                <td>
+                    <span class="badge bg-danger">unpaid</span>
+                </td>
+                <td>Senin, 28 April 2022</td>
+                <td>
+                    <button class="btn btn-warning btn-sm">
+                        <i class="bi bi-pencil-square"></i>
+                    </button>
+                    <button class="btn btn-danger btn-sm">
+                        <i class="bi bi-trash3-fill"></i>
+                    </button>
+                    <button class="btn btn-info btn-sm">
+                        <i class="bi bi-eye-fill"></i>
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
