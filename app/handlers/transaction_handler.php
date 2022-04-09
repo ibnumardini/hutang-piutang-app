0beb4a5ec7cbde8efc53ca2550e9a7b4a35c4dd9
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-04-09 09:04:18 +0700

    add: conditional query for get view transactions

--- app/handlers/transaction_handler.php
+++ app/handlers/transaction_handler.php
@@ -1,6 +1,19 @@
 <?php
 
-$query = mysqli_query($con, "SELECT * FROM transactions");
+if (isset($_GET['view'])) {
+    switch ($_GET['view']) {
+        case 'debt':
+            $where = 'debt';
+            break;
+        default:
+            $where = 'receivable';
+            break;
+    }
+} else {
+    $where = 'debt';
+}
+
+$query = mysqli_query($con, "SELECT * FROM transactions WHERE type = '$where'");
 
 $transactions = [];
 
