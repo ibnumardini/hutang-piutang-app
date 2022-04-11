73dc1ce2c7915f5b5bda41ace96d59e3c3d900cc
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-04-11 09:59:36 +0700

    ref: create  variable

--- app/handlers/transaction_handler.php
+++ app/handlers/transaction_handler.php
@@ -4,13 +4,16 @@ if (isset($_GET['view'])) {
     switch ($_GET['view']) {
         case 'debt':
             $where = 'debt';
+            $title = "Hutang";
             break;
         default:
             $where = 'receivable';
+            $title = "Piutang";
             break;
     }
 } else {
     $where = 'debt';
+    $title = "Hutang";
 }
 
 $query = mysqli_query($con, "SELECT * FROM transactions WHERE type = '$where'");
