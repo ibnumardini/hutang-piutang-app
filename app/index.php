1d8f845260d4e1b9affe2beab92417ab984c1c05
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-04-13 13:22:13 +0700

    ref: routing for create transaction(debt/receivable)

--- app/index.php
+++ app/index.php
@@ -9,7 +9,8 @@ if (!isset($_SESSION['user'])) {
 include_once '../configs/db.php';
 
 include_once './handlers/home_handler.php';
-include_once './handlers/transaction_handler.php';
+include_once './handlers/transactions/transaction_handler.php';
+include_once './handlers/transactions/create_trx_handler.php';
 
 include_once './templates/header.php';
 include_once './templates/navbar.php';
@@ -17,7 +18,18 @@ include_once './templates/navbar.php';
 if (isset($_GET['page'])) {
     switch ($_GET['page']) {
         case 'transactions':
-            include_once './pages/transactions/index.php';
+            if (isset($_GET['action'])) {
+                switch ($_GET['action']) {
+                    case 'create':
+                        include_once './pages/transactions/create.php';
+                        break;
+                    default:
+                        include_once './pages/transactions/index.php';
+                        break;
+                }
+            } else {
+                include_once './pages/transactions/index.php';
+            }
             break;
         default:
             include_once './pages/dashboard.php';
