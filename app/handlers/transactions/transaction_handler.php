a01954652e42d866bfd8fdc1a465317a7b0595cc
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-04-15 10:31:27 +0700

    fix: user relation to persons

--- app/handlers/transactions/transaction_handler.php
+++ app/handlers/transactions/transaction_handler.php
@@ -18,7 +18,7 @@ if (isset($_GET['view'])) {
     $title = "Hutang";
 }
 
-$query = mysqli_query($con, "SELECT * FROM transactions WHERE type = '$where' AND user_id='$session_user_id'");
+$query = mysqli_query($con, "SELECT t.id, t.type, t.user_id, t.use_for, t.person_id, t.nominal, t.temp_nominal, t.status, t.transaction_at, t.due_date, t.created_at, t.updated_at, p.name FROM transactions as t LEFT JOIN persons as p ON t.person_id = p.id WHERE t.type = '$where' AND t.user_id='$session_user_id'");
 
 $transactions = [];
 
--- app/pages/transactions/index.php
+++ app/pages/transactions/index.php
@@ -24,7 +24,7 @@
             <tr>
                 <td><?= $num++ ?></td>
                 <td><?= $transaction['use_for'] ?></td>
-                <td><?= $transaction['person_id'] ?></td>
+                <td><?= $transaction['name'] ?></td>
                 <td><?= $transaction['nominal'] ?></td>
                 <td>
                     <span class="badge bg-danger">
