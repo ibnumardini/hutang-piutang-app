0b6017ca9b04e6c68b15f87c68e834f8471fc526
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-05-21 07:53:45 +0700

    ref: make include handler specific

--- app/handlers/main_handler.php
+++ app/handlers/main_handler.php
@@ -1,5 +1,6 @@
 <?php
 
+$session_user_id = $_SESSION['user']['id'];
 $user_login = $_SESSION['user']['name'];
 
 $page_now = isset($_GET['page']) ? $_GET['page'] : 'home';
--- app/index.php
+++ app/index.php
@@ -8,53 +8,49 @@ if (!isset($_SESSION['user'])) {
 
 include_once '../configs/db.php';
 
-$session_user_id = $_SESSION['user']['id'];
+include_once './handlers/main_handler.php';
 
-include_once './handlers/home_handler.php';
-include_once './handlers/profile/edit_handler.php';
-include_once './handlers/profile/profile_handler.php';
-include_once './handlers/transactions/transaction_handler.php';
-include_once './handlers/transactions/create_trx_handler.php';
+switch ($_GET['page']) {
+    case 'profile':
+        include_once './handlers/profile/edit_handler.php';
+        include_once './handlers/profile/profile_handler.php';
+        break;
+    case 'transactions':
+        include_once './handlers/transactions/transaction_handler.php';
+        include_once './handlers/transactions/create_trx_handler.php';
+        break;
+    default:
+        include_once './handlers/home/home_handler.php';
+        break;
+}
 
 include_once './templates/header.php';
 include_once './templates/navbar.php';
 
-if (isset($_GET['page'])) {
-    switch ($_GET['page']) {
-        case 'profile':
-            if (isset($_GET['action'])) {
-                switch ($_GET['action']) {
-                    case 'edit':
-                        include_once './pages/profile/edit.php';
-                        break;
-                    default:
-                        include_once './pages/profile/index.php';
-                        break;
-                }
-            } else {
+switch ($_GET['page']) {
+    case 'profile':
+        switch ($_GET['action']) {
+            case 'edit':
+                include_once './pages/profile/edit.php';
+                break;
+            default:
                 include_once './pages/profile/index.php';
-            }
-            break;
-        case 'transactions':
-            if (isset($_GET['action'])) {
-                switch ($_GET['action']) {
-                    case 'create':
-                        include_once './pages/transactions/create.php';
-                        break;
-                    default:
-                        include_once './pages/transactions/index.php';
-                        break;
-                }
-            } else {
+                break;
+        }
+        break;
+    case 'transactions':
+        switch ($_GET['action']) {
+            case 'create':
+                include_once './pages/transactions/create.php';
+                break;
+            default:
                 include_once './pages/transactions/index.php';
-            }
-            break;
-        default:
-            include_once './pages/dashboard.php';
-            break;
-    }
-} else {
-    include_once './pages/dashboard.php';
+                break;
+        }
+        break;
+    default:
+        include_once './pages/dashboard.php';
+        break;
 }
 
 include_once './templates/footer.php';
