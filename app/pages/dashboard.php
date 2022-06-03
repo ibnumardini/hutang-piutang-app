6b0dbe19c44fccfbff6a2d793745371b032f7041
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-06-03 08:17:08 +0700

    feat: dashboard layouting

--- app/pages/dashboard.php
+++ app/pages/dashboard.php
@@ -1,3 +1,74 @@
 <div class="container">
     <h1>Dashboard</h1>
+
+    <div class="row mt-4">
+        <div class="col-md-3">
+            <div class="card text-white bg-success mb-3" style="height: 170px">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Jumlah Hutang</h5>
+                    <hr>
+                    <p class="card-text d-flex justify-content-center align-items-center">
+                        <span style="font-size: 2rem" class="fw-bold">1.000.000,00</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-success mb-3" style="height: 170px">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Jumlah Piutang</h5>
+                    <hr>
+                    <p class="card-text d-flex justify-content-center align-items-center">
+                        <span style="font-size: 2rem" class="fw-bold">1.000.000,00</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-success mb-3" style="height: 170px">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Jumlah Trx Hutang</h5>
+                    <hr>
+                    <p class="card-text d-flex justify-content-center align-items-center">
+                        <span class="display-4 fw-bold">10</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-success mb-3" style="height: 170px">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Jumlah Trx Piutang</h5>
+                    <hr>
+                    <p class="card-text d-flex justify-content-center align-items-center">
+                        <span class="display-4 fw-bold">10</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-success mb-3" style="height: 170px">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Paling Sering Hutang</h5>
+                    <hr>
+                    <ol>
+                        <li>Fulan bin Fulan</li>
+                    </ol>
+                    <a href="#" class="btn btn-link text-white">More..</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-white bg-success mb-3" style="height: 170px">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Paling Sering Piutang</h5>
+                    <hr>
+                    <ol>
+                        <li>Fulan bin Fulan</li>
+                    </ol>
+                    <a href="#" class="btn btn-link text-white">More..</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
