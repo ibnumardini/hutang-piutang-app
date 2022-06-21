7d9d757f33e0e188a891ed32c07c08a5cf4b7e66
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-06-21 08:16:34 +0700

    feat: add person detail

--- app/handlers/persons/person_handler.php
+++ app/handlers/persons/person_handler.php
@@ -17,3 +17,25 @@ if (isset($_GET['view'])) {
     $where = 'debt';
     $title = "Hutang";
 }
+
+$query = "SELECT DISTINCT(p.name) FROM transactions as t LEFT JOIN persons as p ON t.person_id = p.id WHERE t.type = '$where' AND t.user_id = '$session_user_id'";
+
+if (isset($_GET['sort'])) {
+    switch ($_GET['sort']) {
+        case 'za':
+            $sort = "Z - A";
+            $query .= " ORDER BY p.name DESC";
+            break;
+        default:
+            $sort = "A - Z";
+            $query .= " ORDER BY p.name ASC";
+            break;
+    }
+}
+
+$trx = mysqli_query($con, $query);
+
+$personsTrx = [];
+while ($row = mysqli_fetch_assoc($trx)) {
+    array_push($personsTrx, $row);
+}
--- app/pages/persons/index.php
+++ app/pages/persons/index.php
@@ -1,3 +1,29 @@
 <div class="container">
-    <h2>List Person <?= $title ?></h2>
+    <div class="row">
+        <div class="col-md-6">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/app/index.php">Dashboard</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">List Person <?=$title?></li>
+                </ol>
+            </nav>
+            <h2 class="mb-4">List Person <?=$title?></h2>
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Sort <?= $sort ? "~ " . $sort : ""?>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="/app/index.php?page=most-freq-trx&view=<?=$where?>&sort=az">A - Z</a></li>
+                    <li><a class="dropdown-item" href="/app/index.php?page=most-freq-trx&view=<?=$where?>&sort=za">Z - A</a></li>
+                </ul>
+            </div>
+            <div class="card mt-4" style="width: 18rem;">
+                <ul class="list-group list-group-flush">
+                    <?php $i = 1;foreach ($personsTrx as $trx): ?>
+                        <li class="list-group-item"><?=$i++ . ".  " . ucfirst($trx['name'])?></li>
+                    <?php endforeach;?>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
